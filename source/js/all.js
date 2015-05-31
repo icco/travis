cc0d8b316bef83bc6602ae06ab28b81de50fa0c9
Nat Welch <nat@natwelch.com>
2015-05-31 20:37:00 +0000

    things looking good

--- source/js/all.js
+++ source/js/all.js
@@ -1,1 +1,8 @@
-//= require_tree .
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var container = document.querySelector('#container');
+  var msnry;
+  // initialize Masonry after all images have loaded
+  imagesLoaded(container, function() {
+    msnry = new Packery(container, {});
+  });
+});
